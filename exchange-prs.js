03ce55d06066148259fb362a66b84ad7e311e31c
Nicolás Escalante <nlante@gmail.com>
2016-12-01 15:44:43 -0300

    Update exchange-prs.js

--- exchange-prs.js
+++ exchange-prs.js
@@ -1,6 +1,40 @@
+var fetch = require('isomorphic-fetch');
+
 module.exports = function (ctx, cb) {
-  cb(null, {
-    response_type: 'in_channel',
-    text: 'Hello from @' + ctx.body.user_name + '!',
+  var token = ctx.data['github-token'];
+  var prsPromise = [
+    'mulesoft/exchange-ui'
+  ].map(function (repo) {
+    return fetchPRs(repo);
   });
+  
+  Promise.all(prsPromise)
+    .then(function (prs) { 
+      return prs.reduce(function(a, b) {
+        return a.concat(b);
+      }, []);
+    })
+    .then(function (prs) {
+      cb(null, {
+        // response_type: 'in_channel', // uncomment to have the response visible to everyone on the channel
+        text: JSON.stringify(prs) + 'Hello, @' + ctx.body.user_name + '!'
+      });
+    })
+    .catch(function (err) {
+      cb(null, {
+        // response_type: 'in_channel', // uncomment to have the response visible to everyone on the channel
+        text: err.message
+      });
+    });
+
+  function fetchPRs(repo) {
+    var headers = {
+      Authorization: 'token ' + token
+    };
+    
+    return fetch('https://api.github.com/repos/' + repo + '/pulls', { headers: headers })
+      .then(function (response) {
+        return response.json();
+      });
+  }
 };
