c0629dbd3db3eb2adfbce2e1680dc0f9bee4508c
Nicolás Escalante <nlante@gmail.com>
2016-12-02 14:05:54 -0300

    time ago

--- exchange-prs.js
+++ exchange-prs.js
@@ -30,15 +30,27 @@ module.exports = function (ctx, cb) {
           return new Date(a.updated_at) - new Date(b.updated_at);
         })
         .map(function (pr) {
+          const ms = new Date() - new Date(pr.updated_at);
+          const secs = ms / 1000;
+          const mins = secs / 60;
+          const hours = mins / 60;
+          let lastUpdated;
+          
+          if (hours < 1) {
+            lastUpdated = Math.round(mins) + ' minutes ago';
+          } else {
+            lastUpdated = Math.round(hours) + ' hours ago';
+          }
+          
           return {
             fallback: pr.html_url,
-            color: "#36a64f",
-            pretext: "Optional text that appears above the attachment block",
+            color: "#eee",
             author_name: pr.user.login,
             author_link: pr.user.html_url,
             author_icon: pr.user.avatar_url,
             title: pr.title + ' in ' + pr.base.repo.name,
-            title_link: pr.html_url
+            title_link: pr.html_url,
+            text: 'Updated ' + lastUpdated + '.'
           };
           // return '> :pushpin: ' + pr.title + '\n> :link: ' + pr.html_url + '\n> :speaking_head_in_silhouette: ' + pr.user.login + '\n';
         });
