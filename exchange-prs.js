8ebc02a806f6785a7d26b917f6c5e5ade1693b64
Nicolás Escalante <nlante@gmail.com>
2016-12-12 13:24:10 -0300

    Update exchange-prs.js

--- exchange-prs.js
+++ exchange-prs.js
@@ -11,6 +11,7 @@ module.exports = function (ctx, cb) {
     'mulesoft/asset-portal-service-db',
     'mulesoft/asset-review-service',
     'mulesoft/asset-review-service-db',
+    'mulesoft/exchange-graph-service',
     'mulesoft/exchange-maven-facade',
     'mulesoft/exchange-ui',
     'mulesoft/exchange-xapi',
